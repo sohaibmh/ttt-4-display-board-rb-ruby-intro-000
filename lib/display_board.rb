0a1cace0dbd19c7eb42fb8efd8c5025c4b1bac9b
Sohaib Mubashir <sohaibmh@yahoo.com>
2019-03-31 12:38:40 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -8,6 +8,12 @@ def display_board(array)
   end
 end
 
+puts " #{array[1]}  | #{array[2]}  | #{array[3]}  "
+puts "-----------"
+puts " #{array[4]}  | #{array[5]}  | #{array[6]}  "
+puts "-----------"
+puts " #{array[7]}  | #{array[8]}  | #{array[9]}  "
+
 moves = []
 
 display_board(moves)
