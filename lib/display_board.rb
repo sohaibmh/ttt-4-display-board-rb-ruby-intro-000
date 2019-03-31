4d6f1e68fc82c9e3e75896578af27462786e3ec2
Sohaib Mubashir <sohaibmh@yahoo.com>
2019-03-31 12:45:06 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -8,9 +8,9 @@ def display_board(array)
 else
   puts " #{array[0]} | #{array[1]} | #{array[2]} "
   puts "-----------"
-  puts " #{array[3]}  | #{array[4]}  | #{array[5]}  "
+  puts " #{array[3]} | #{array[4]} | #{array[5]} "
   puts "-----------"
-  puts " #{array[6]}  | #{array[7]}  | #{array[8]}  "
+  puts " #{array[6]} | #{array[7]} | #{array[8]} "
   end
 end
 
